eeedf862a57c846f30406f70c3afd129cb391d1d
Jonathan <chevalier.jonathan@live.Fr>
2024-03-11 15:34:50 -0300

    wip

--- backend/params.py
+++ backend/params.py
@@ -6,7 +6,7 @@ MODEL_TARGET = os.environ.get("MODEL_TARGET")
 BUCKET_NAME = os.environ.get("BUCKET_NAME")
 
 MLFLOW_MODEL_NAME = os.environ.get("MLFLOW_MODEL_NAME")
-
+MLFLOW_TRACKING_URI = os.environ.get("MLFLOW_TRACKING_URI")
 ##################  CONSTANTS  #####################
 LOCAL_REGISTRY_PATH =  os.path.join(os.path.expanduser('~'), ".lewagon", "final-project", "training_outputs")
 

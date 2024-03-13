47ba2e054aa7583a0aef2f37a9325382dd503498
Lorena Santos <lorenamelos1@gmail.com>
2024-03-13 10:16:32 -0300

    app last version 12/03

--- python/musicbrain/app/pages/userinterface.py
+++ python/musicbrain/app/pages/userinterface.py
@@ -1,30 +1,54 @@
 
 import streamlit as st
-import joblib
+from joblib import load
+import pandas as pd
 import numpy as np
 
 
 
-
-#pipeline = build_pipeline()
-
 # Load the trained model
-pipeline = joblib.load('OnevsRest.pkl')
+pipeline = load('OnevsRest.joblib')
 
 # Define the user interface
 st.title('Music Genre Classification')
-st.write('Enter vectorized data to classify its music genre:')
+st.write('Upload a CSV file containing vectorized data to classify its music genre:')
 
-# Input fields for user to enter data
-input_data = st.text_area("Enter vectorized data (comma-separated)", height=100)
+# Upload CSV file
+uploaded_file = st.file_uploader("Choose a CSV file...", type=["csv"])
 
-# Button to trigger prediction
-if st.button('Predict'):
-    # Convert input string to numpy array
-    input_array = np.fromstring(input_data, sep=',')
-    input_array = input_array.reshape(1, -1)  # Reshape to match the expected shape for prediction
+if uploaded_file is not None:
+    # Read the CSV file as a pandas DataFrame
+    df = pd.read_csv(uploaded_file)
 
-    # Make prediction using the model
-    prediction = pipeline.predict(input_array)
+    # Display the head of the uploaded data
+    st.write('Head of Uploaded Data:')
+    st.write(df.head())
 
-    st.write('Predicted Music Genre:', prediction[0])
+    # Extract vectorized data from the DataFrame
+    input_data = df.values
+
+    # Button to trigger prediction
+    if st.button('Predict'):
+        # Add a progress bar
+        progress_bar = st.progress(0)
+
+        # Function to make prediction
+        def make_prediction(input_data):
+            prediction = pipeline.predict(input_data)
+            return prediction
+
+        # Make prediction using the model
+        prediction = make_prediction(input_data)
+
+        # Update progress bar
+        progress_bar.progress(100)
+
+        # Create a DataFrame with prediction results
+        prediction_df = pd.DataFrame({
+                        'prediction': prediction
+        })
+
+
+        # Display the prediction results DataFrame
+        st.write('Prediction Results:')
+        st.dataframe(prediction_df, height=200, width=300)  # Set the height and width for the prediction results DataFrame

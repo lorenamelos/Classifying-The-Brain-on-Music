bb31a2296f59a2060e2e0ea1b6321749b9cef8c6
Lorena Santos <lorenamelos1@gmail.com>
2024-03-13 16:51:23 -0300

    modifications on loading model on front end

--- musicbrain/musicbrain-front/userinterface.py
+++ musicbrain/musicbrain-front/userinterface.py
@@ -1,54 +1,61 @@
 
+# Import necessary libraries
+import json
 import streamlit as st
-from joblib import load
+import requests
 import pandas as pd
 import numpy as np
 
 
+# Define the URL of your Fast API
 
-# Load the trained model
-pipeline = load('OnevsRest.joblib')
-
-# Define the user interface
-st.title('Music Genre Classification')
-st.write('Upload a CSV file containing vectorized data to classify its music genre:')
-
-# Upload CSV file
-uploaded_file = st.file_uploader("Choose a CSV file...", type=["csv"])
-
-if uploaded_file is not None:
-    # Read the CSV file as a pandas DataFrame
-    df = pd.read_csv(uploaded_file)
-
-    # Display the head of the uploaded data
-    st.write('Head of Uploaded Data:')
-    st.write(df.head())
-
-    # Extract vectorized data from the DataFrame
-    input_data = df.values
-
-    # Button to trigger prediction
-    if st.button('Predict'):
-        # Add a progress bar
-        progress_bar = st.progress(0)
-
-        # Function to make prediction
-        def make_prediction(input_data):
-            prediction = pipeline.predict(input_data)
-            return prediction
-
-        # Make prediction using the model
-        prediction = make_prediction(input_data)
-
-        # Update progress bar
-        progress_bar.progress(100)
-
-        # Create a DataFrame with prediction results
-        prediction_df = pd.DataFrame({
-                        'prediction': prediction
-        })
+API_URL = 'http://127.0.0.1:8000/predict'
 
 
-        # Display the prediction results DataFrame
-        st.write('Prediction Results:')
-        st.dataframe(prediction_df, height=200, width=300)  # Set the height and width for the prediction results DataFrame
+# Create the Streamlit app
+def main():
+    st.title('Music Genre Classification')
+    st.write('Upload a CSV file containing vectorized data to classify its music genre:')
+
+    # Upload CSV file
+    uploaded_file = st.file_uploader("Choose a CSV file...", type=["csv"])
+
+    if uploaded_file is not None:
+        # Read the CSV file as a pandas DataFrame
+        df = pd.read_csv(uploaded_file)
+
+        # Display the head of the uploaded data
+        st.write('Head of Uploaded Data:')
+        st.write(df.head())
+
+        # Button to trigger prediction
+        if st.button('Predict'):
+            # Add a progress bar
+            progress_bar = st.progress(0)
+
+            # Function to make prediction using the API
+            def make_prediction(input_data):
+                # Convert NumPy array to list
+                input_data_list = input_data.tolist()
+                # Send the request with JSON serializable data
+                response = requests.post(API_URL, json={'data': input_data_list})
+                if response.status_code == 200:
+                    return response.json()['prediction']
+                else:
+                    return None
+
+            # Make prediction using the API
+            prediction = make_prediction(df.values)
+
+            # Update progress bar
+            progress_bar.progress(100)
+
+            # Create a DataFrame with prediction results
+            if prediction:
+                prediction_df = pd.DataFrame({'prediction': prediction})
+                # Display the prediction results DataFrame
+                st.write('Prediction Results:')
+                st.dataframe(prediction_df, height=200, width=300)  # Set the height and width for the prediction results DataFrame
+
+if __name__ == '__main__':
+    main()

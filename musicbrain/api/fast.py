44d925d8b47ae6d18e3b3b234556def3e98cfdd6
Jonathan <chevalier.jonathan@live.Fr>
2024-03-14 10:39:45 -0300

    wip

--- musicbrain/api/fast.py
+++ musicbrain/api/fast.py
@@ -1,8 +1,10 @@
 import io
+import json
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 
 import pandas as pd
+from pydantic import BaseModel
 
 from musicbrain.ml_logic.model import int_to_music_label
 from musicbrain.ml_logic.registry import load_model
@@ -18,8 +20,23 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/predict")
-async def predict(file: UploadFile = File(None), json_data: dict = None):
+@app.get("/predict/json")
+async def predict(data: dict = None):
+    if data:
+        try:
+            X_pred = pd.DataFrame(data)
+            y_pred = app.state.model.predict(X_pred)
+            
+            result = int_to_music_label(y_pred)
+            return {"music_labels": result}
+        except Exception as e:
+            return {"error": f"Invalid JSON, exception '{e}'"}
+        
+    else:
+        return {"error": "No valid input provided"}
+
+@app.get("/predict/csv")
+async def predict(file: UploadFile = File(None)):
     
     if file and file.filename.endswith((".csv")):
         contents = await file.read()
@@ -32,16 +49,6 @@ async def predict(file: UploadFile = File(None), json_data: dict = None):
             return {"music_labels": result}
         except Exception as e:
             return {"error": f"Invalid CSV file, exception '{e}'"}
-
-    elif json_data:
-        try:
-            X_pred = pd.read_json(json_data)
-            y_pred = app.state.model.predict(X_pred)
-            
-            result = int_to_music_label(y_pred)
-            return {"music_labels": result}
-        except Exception as e:
-            return {"error": f"Invalid JSON, exception '{e}'"}
-        
+  
     else:
         return {"error": "No valid input provided"}
